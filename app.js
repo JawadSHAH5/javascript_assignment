93afff7a8bb48edf598bb29d857dd96d64606790
JawadSHAH5 <142098487+JawadSHAH5@users.noreply.github.com>
2023-10-18 03:24:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,162 @@
+// chapter no 1
+
+
+
+// question no 1
+       
+// alert("saylani mass IT");
+
+
+
+
+// question no 2
+
+// alert("Error! please enter a valid password");   
+
+
+
+
+// question no 3
+
+// alert("welcome to JS land... \n  happy coding!");
+
+
+
+
+// question no 4
+
+// alert("welcome to JS land");
+
+
+
+
+
+
+// chapter no 2
+
+
+
+
+// question no 1
+
+// var name = "username";
+
+// alert(name)
+
+
+
+
+// question no 2 
+
+// var myName = "syed jawad shah";
+
+// alert(myName)
+
+
+
+
+// question no 3 
+
+// var name = "johne deo";
+// var age = 15;
+// var sec = "web and app";
+
+
+// alert(name)
+// alert(age)
+// alert(sec)
+    
+
+
+
+
+// question no 4 
+
+// var message = "hello world";
+
+// alert(message)
+
+
+
+
+// question no 5 
+
+// var a ="pizza";
+// var b ="pizz";
+// var c ="piz";
+// var d ="pi";
+// var e ="p";
+
+
+// alert(a)
+// alert(b)
+// alert(c)
+// alert(d)
+// alert(e)
+
+
+
+
+// question no 6
+
+// var email = "my email address is jshah6229@gmail.com";
+
+// alert(email)
+
+
+
+
+
+// chapter no 3
+
+// question no 1
+
+// var age = "my age is 17 year old";
+
+// alert(age)
+
+
+
+
+
+
+// question no 2 
+
+
+// var visit ="you have visited this site 14 times";
+
+// alert(visit)
+
+
+
+
+// question no 3 
+
+
+// var birthYear = "my birth year is 2002";
+
+// alert(birthYear)
+
+
+
+
+// question no 4 
+
+// var name = "jawad";
+// var product ="phone";
+// var quantity=5;
+
+
+// document.write('welcome ' +name+ 'you have bought '+quantity+'of '+product +'from us at store')
+
+
+
+
+// chapter no 4 
+
+// question no 1
+
+var a=10 ,b=5 ,c=15;
+
+console.log(a,b,c)
+
