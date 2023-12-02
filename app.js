18c095340db0cfe8671074d18b2678637ff37204
Syed Jawad Shah <142098487+JawadSHAH5@users.noreply.github.com>
2023-12-02 18:58:21 +0500

    javascript task

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-var username = prompt("enter user name")
-var phoneNumber = +prompt("enter phoneNumber")
-var emailAddress = prompt("enter email address")
-var password = prompt("Please enter your password: ")
-var confirmPassword = prompt("Confirm Password:")
 
-if(username==""|| phoneNumber == ""||emailAddress== ""||password==""||confirmPassword==""){
-    alert('All fields must be filled out')
+
+
+
+function isPalindrome(str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+    // Reverse the cleaned string
+    const reversedStr = cleanedStr.split('').reverse().join('');
+
+    // Check if the original and reversed strings are the same
+    return cleanedStr === reversedStr;
 }
 
-if(password===confirmPassword){
-    alert("Welcome , Your account has been created. Please log in to continue")
-
+// Example usage:
+const userInput = prompt("Enter a string to check if it's a palindrome:");
+if (userInput) {
+    if (isPalindrome(userInput)) {
+        alert(`${userInput} is a palindrome!`);
+    } else {
+        alert(`${userInput} is not a palindrome.`);
+    }
 } else {
-    alert("enter same password")
+    alert("Please enter a string.");
 }
